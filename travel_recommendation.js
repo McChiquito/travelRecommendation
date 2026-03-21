72d4077600d76e0bf2253a1adc6453de470b8beb
McChiquito <alicyahir02@gmail.com>
2026-03-20 23:03:38 -0600

    termino de proyecto para evaluacion

---
+++ travel_recommendation.js
@@ -0,0 +1,69 @@
+fetch("./travel_recommendation.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.log(error));
+
+let travelData = {};
+
+fetch("./travel_recommendation.json")
+    .then(res => res.json())
+    .then(data => {
+        travelData = data;
+    });
+
+document.getElementById("searchBtn").addEventListener("click", function(){
+    let keyword = document.getElementById("searchInput").value.toLowerCase();
+
+    let resultsContainer = document.getElementById("results");
+    resultsContainer.innerHTML = "";
+
+    document.getElementById("results").scrollIntoView({
+        behavior: "smooth"
+    });
+    if(keyword.includes("country")){
+        travelData.countries.forEach(country => {
+            country.cities.forEach(city => {
+                resultsContainer.innerHTML += `
+                    <div class="result-card">
+                        <img src="${city.imageUrl}" width="300">
+                        <h3>${city.name}</h3>
+                        <p>${city.description}</p>
+                    </div>
+                `;
+            });
+        });
+    }
+if(keyword.includes("temple")){
+    travelData.temples.forEach(temple=>{
+        resultsContainer.innerHTML += `
+            <div class="result-card">
+                <img src="${temple.imageUrl}" width="300">
+                <h3>${temple.name}</h3>
+                <p>${temple.description}</p>
+            </div>
+        `;
+    });
+}
+
+if(keyword.includes("beach")){
+    travelData.beaches.forEach(beach=>{
+        resultsContainer.innerHTML += `
+            <div class="result-card">
+                <img src="${beach.imageUrl}" width="300">
+                <h3>${beach.name}</h3>
+                <p>${beach.description}</p>
+            </div>
+        `;
+    });
+}
+});
+
+
+document.getElementById("clearBtn").addEventListener("click", function() {
+    // Limpiar resultados
+    document.getElementById("results").innerHTML = "";
+    // Limpiar input
+    document.getElementById("searchInput").value = "";
+})
